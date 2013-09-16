2a9294b945874b8829751a79cbc16cb2d3c33bcb
James Singh <james.singh@aceit.com.np>
2013-09-16 15:01:41 +0545

    Module to register new instances to Nagios on startup

--- app/controllers/maincontroller.js
+++ app/controllers/maincontroller.js
@@ -4,11 +4,17 @@ var app = express();
 var mongoose = require('mongoose');
 var url = require("url");
 var Hostname = mongoose.model('Hostname');
+var fs = require('fs');
+
+var env = process.env.NODE_ENV || 'development'
+var config = require('../../config/config')[env]
+var exec = require('child_process').exec;
 
 exports.getHostname = function(req, res){
   var q = url.parse(req.url,true).query;
   console.log("Q: "+JSON.stringify(q));
   var domain = q.domain;
+  var private_ip = q.private_ip;
   var name = domain.split(".")[0];
   console.log("Domain: "+domain)
   Hostname.findOne({"domain":domain}, function(err, data){
@@ -33,8 +39,10 @@ exports.getHostname = function(req, res){
         if(err) throw err;
       });
     }
-    var hostname = name+next+"."+domain;
+    var alias = name+next;
+    var hostname = alias+"."+domain;
     console.log("Output: "+hostname);
+    configNagios(domain, private_ip, alias);
     res.write(hostname);
     res.end();
   })
@@ -44,4 +52,25 @@ exports.list = function(req, res){
   Hostname.find({}, function(err, domains){
     res.json(domains);
   })
+}
+
+function configNagios(domain, private_ip, alias){
+	var filename = domain.replace(/\./g,'_');
+	fs.readFile(config.nagios_config_template_path+filename+'.cfg', 'utf8', function (err,data) {
+		if (err) {
+			return console.log(err);
+		}
+		data = data.replace(/#PRIVATE_IP#/g, private_ip);
+		data = data.replace(/#ALIAS#/g,alias);	
+		fs.writeFile(config.nagios_config_path+alias+'.cfg', data, 'utf8', function (err) {
+			if (err) return console.log(err);
+			exec("service nagios restart", function(error, stdout, stderr){
+				if(error){
+					console.log("Error restarting nagios");
+				} else {
+					console.log("Nagios restarted successfully");
+				}
+			});
+		});
+	});
 }
