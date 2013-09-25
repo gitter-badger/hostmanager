61b694d99746e48025d220b00461fa493e7f8abc
James Singh <james@icloudtech.com>
2013-09-25 11:13:23 +0545

    Changing dir path to a possibly correct one

--- config/config.js
+++ config/config.js
@@ -5,8 +5,8 @@ module.exports = {
   development: {
     db: 'mongodb://dbm.cloud.acetravels.com/hostmaster'
     , root: rootPath
-    , nagios_config_template_path: '../../templates/nagios-templates/'
-    , mrtg_config_template_path: '../../templates/mrtg-templates/'
+    , nagios_config_template_path: './templates/nagios-templates/'
+    , mrtg_config_template_path: './templates/mrtg-templates/'
     , nagios_config_path: '/etc/nagios3/conf.d/'
     , mrtg_config_html_folder_path: '/var/www/html/mrtg/'
     , mrtg_path: '/opt/mrtg/'
