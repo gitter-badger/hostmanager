6cfe94c52875c4dd82f0130bcdc01547520327d8
James Singh <james@icloudtech.com>
2013-09-25 12:25:23 +0545

    missing url :)

--- config/config.js
+++ config/config.js
@@ -23,6 +23,6 @@ module.exports = {
     , mrtg_config_html_folder_path: '/var/www/html/mrtg/'
     , mrtg_path: '/opt/mrtg/'
     , mrtg_side_php_path: '/usr/share/nagios3/htdocs/side.php'
-    , mrtg_host_url: 'http://#DOMAIN_NAME#/html/mrtg/#ALIAS#/index.html'
+    , mrtg_host_url: 'http://config-server.cloud.acetravels.com/html/mrtg/#ALIAS#/index.html'
   }
 }
