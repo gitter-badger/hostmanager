5e6bf2851684ed510ff9f71268cc29c500fa44fa
James Singh <james@icloudtech.com>
2013-10-03 14:21:07 +0545

    x

--- config/setupcrontab.js
+++ config/setupcrontab.js
@@ -1,12 +1,12 @@
 var exec = require('child_process').exec;
 
 exports.setup = function(){  
-  exec("crontab -l | grep cleanup", function(error, stdout, stderr){
+  exec("crontab -l", function(error, stdout, stderr){
     if(error){
       console.log("Error listing crontab");
     } else {
       console.log("Stdout: "+stdout);
-      if(stdout==null || stdout==""){
+      if(stdout.indexOf("cleanup") < 0){
         exec("(crontab -l ; echo \"*/5 * * * * root curl http://config-server.cloud.acetravels.com:3000/cleanup\") | crontab -", function(error, stdout, stderr){
           if(error){
 	        console.log("Could not setup crontab for periodic cleanup");
